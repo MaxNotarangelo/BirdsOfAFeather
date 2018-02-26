1200a4299c9116fe3ab0eab3d65a18ad3c10f274
clockworkdevelopment <Max>
2018-02-26 08:27:26 -0800

    String getters
    Added several methods to get different properties of a card as a string, including the __str__() method, which returns a two-character result which is intended to be printed to the screen (e.g. A♦).

--- Card.py
+++ Card.py
@@ -4,6 +4,11 @@ class Card:
     ranks = {'A': 0, '2': 1, '3': 2, '4': 3, '5': 4, '6': 5, '7': 6, '8': 7, '9': 8, 'T': 9, 'J': 10, 'Q': 11, 'K': 12 }
     suits = {'C': 0, 'D': 1, 'S': 2, 'H': 3}
 
+    rankNames = ['A', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K']
+    suitNames = ['C', 'D', 'S', 'H']
+    suitSymbols = ['♣', '♦', '♠', '♥']
+
+    # Constructors
     def __init__(self, rank, suit):
         # Assign rank
         if type(rank) == str:
@@ -25,6 +30,7 @@ class Card:
         self.rank = id % 13
         self.suit = int(id / 13)
 
+    # Integer getters
     def get():
         return (self.rank, self.suit)
 
@@ -37,4 +43,27 @@ class Card:
     def getid():
         return self.suit * 13 + self.rank
 
-# This comment tests whether I can push to github
+    def isSuitRed():
+        if self.suit == 0 or self.suit == 2:
+            return False
+        else:
+            return True
+
+    # String getters
+    def __str__(self):
+        return rankNames[self.rank] + suitNames[self.suit]
+
+    def getRankStr():
+        return rankNames[self.rank]
+
+    def getSuitStr():
+        return suitNames[self.suit]
+
+    def write():
+        return rankNames[self.rank] + suitSymbols[self.suit]
+
+    def writeRank():
+        return self.getRankStr()
+
+    def writeSuit():
+        return suitSymbols[self.suit]
